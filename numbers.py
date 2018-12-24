c7c4be1973d4312bdf4adb079fd12728df8c70d8
Muhammad Sidiq Putra <muhammadsidiqputra@Muhammads-Sidiq.local>
2018-12-24 23:07:56 +0700

    first commit python

---
+++ numbers.py
@@ -0,0 +1,10 @@
+#there is 3 type of number in python
+#Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length.
+#Float, or "floating point number" is a number, positive or negative, containing one or more decimals.
+# Complex numbers are written with a "j" as the imaginary part
+x = 5 
+y = 1.5
+z = 1j
+print(type(x))
+print(type(y))
+print(type(z))
