ca034f95cc68957c47a44f202992314cd8c49883
Muhammad Sidiq Putra <muhammadsidiqputra@Muhammads-Sidiq.local>
2018-12-24 23:36:03 +0700

    new

---
+++ string.py
@@ -0,0 +1,23 @@
+x = "hello world"
+print(x[0])
+
+y = "substring test"
+print(y[0:11])
+
+z = " strip remove "
+print(z.strip())
+
+j = "length of word"
+print(len(j))
+
+w = "LoWeRCase"
+print(w.lower())
+
+a = "uppercase"
+print(a.upper())
+
+b = "replace kata"
+print(b.replace("kata", "word"))
+
+c = "split word with space"
+print(c.split(" "))
