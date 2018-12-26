bb82e5008c5c71aa467ba4b69f15870c671a86e5
Muhammad Sidiq Putra <muhammadsidiqputra@Muhammads-Sidiq.local>
2018-12-26 20:19:01 +0700

    s3-upload update with slack notif

--- s3-upload.py
+++ s3-upload.py
@@ -1,12 +1,47 @@
+import os
 import boto3
+from datetime import datetime, timedelta
+import slackweb
 
-bucketName = "your bucketname"
-Key = "file to upload"
-remotename = "filename after uploaded"
+filedir = "./"
+todayfile = datetime.now().strftime('%Y%m%d')
+oldfile = (datetime.now() - timedelta(days=2)).strftime('%Y%m%d')
+extfile = ".000000.gz"
 
+
+bucketName = "mybucket"
+filetoupload = filedir+todayfile+extfile
+filetodelete = oldfile+extfile
+remoteName = todayfile+extfile
+
+#slack environment
+slackchannel = '#testchannel'
+successnotif = "file success to upload"
+failednotif = "file failed to upload"
+slackuser = "test-bot"
+slackemoji = ":cactus:"
+slack = slackweb.Slack(url="https://hooks.slack.com/services/0000000000/00000000/XXXXXXXXXXXXXXXXX")
+
+
+#upload file to s3
 try :
- s3 = boto3.client('s3')
- s3.upload_file(Key,bucketName,remotename)
- print(Key + " Uploaded")
+   s3 = boto3.client('s3')
+   s3.upload_file(filetoupload,bucketName,remoteName)
+   slack.notify(text=successnotif, channel=slackchannel, username=slackuser, icon_emoji=slackemoji)
+   print(filetoupload + " Uploaded")
 except:
- print("Failed to upload")
+   slack.notify(text=failednotif, channel=slackchannel, username=slackuser, icon_emoji=slackemoji)
+   print("Failed to upload")
+
+#delete old file on s3
+try :
+   s3.delete_object(
+     Bucket=bucketName,
+     Key=filetodelete,
+   )
+   print("delete old file success")
+except :
+   print("delete old file failed")
+
+#remove backup file on local
+os.remove(filetoupload)
